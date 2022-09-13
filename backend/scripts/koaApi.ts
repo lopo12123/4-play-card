1442afa79e98b1b9fb1f24cf41fdf221a5ad7983
lopo <lopo@zju.edu.cn>
2022-09-13 15:22:11 +0800

    style: 注释调整

--- backend/scripts/koaApi.ts
+++ backend/scripts/koaApi.ts
@@ -8,18 +8,23 @@ const apiRouter = new Router({
 })
 
 apiRouter
+    // ws 连接数量
     .get('/ws/wsCount', ctx => {
         ctx.response.body = WSController.wsCount()
     })
+    // room 数量
     .get('/ws/roomCount', ctx => {
         ctx.response.body = WSController.roomCount()
     })
+    // ws id列表
     .get('/ws/wsList', ctx => {
         ctx.response.body = WSController.wsList()
     })
+    // room id列表
     .get('/ws/roomList', ctx => {
         ctx.response.body = WSController.roomList()
     })
+    // room 信息列表 {roomId: string, count: number}[]
     .get('/ws/roomInfo', ctx => {
         const ids = ctx.request.query['ids'] as string[] | undefined
         ctx.response.body = WSController.roomInfo(ids)
